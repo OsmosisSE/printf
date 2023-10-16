8014e1c82b503896d827bc538eae01f320a08749
OsmosisSE <sirosmosis1@gmail.com>
2023-10-15 18:04:04 -0700

    updated

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int main()
+{
+	printf("standard output\n");
+	printf("Hello world");
+
+	printf("\n.....................\n");
+	
+	printf("Home made output\n");
+	_printf("Hello world!\n");
+
+	return (0);
+}
