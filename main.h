72e22e1502e421659ae4a479a0d483811a39d746
OsmosisSE <sirosmosis1@gmail.com>
2023-10-17 00:47:33 -0700

    updated

--- main.h
+++ main.h
@@ -12,6 +12,26 @@
 /* buffer */
 #define BUFF_SIZE 1024
 
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
 
 /* structure */
 /**
